05204102f821c7970b33bcf1de1552af52494de5
nehasharma <nehasharma@webhopers.in>
2022-10-14 17:40:09 +0530

    Updates

---
+++ precache-manifest.01134b1040291167fafc3aa0f8c44777.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13b23e257fe30e995ad0",
+    "url": "/React-html-website/static/js/main.13b23e25.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/React-html-website/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "9e04273a008ca4587e14",
+    "url": "/React-html-website/static/js/2.9e04273a.chunk.js"
+  },
+  {
+    "revision": "a50e09fd9e8bccec304f",
+    "url": "/React-html-website/static/js/runtime~main.a50e09fd.js"
+  },
+  {
+    "revision": "9a41b9a8bd803efeb67781f874285c33",
+    "url": "/React-html-website/index.html"
+  }
+];
